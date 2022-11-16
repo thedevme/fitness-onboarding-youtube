611df09b7f26cf1162e31c0d60e8997aa5bcfda4
Craig Clayton <craig@designtoswiftui.io>
2022-11-16 13:24:52 -0500

    Part 2 Files added

---
+++ completed chapters/part2/FitnessOnboarding/FitnessOnboarding/Utilities/Fonts/CustomFont.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+enum CustomFont: String {
+    case black = "BarlowSemiCondensed-Black"
+    case bold = "BarlowSemiCondensed-Bold"
+    case medium = "BarlowSemiCondensed-Medium"
+}
---
+++ completed chapters/part2/FitnessOnboarding/FitnessOnboarding/Views/PersonalInfoView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct PersonalInfoView: View {
+    var body: some View {
+        Text("Personal Info View")
+    }
+}
+
+struct PersonalInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonalInfoView()
+    }
+}
